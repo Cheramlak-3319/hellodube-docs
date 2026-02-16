ffd344787763fa5417ced623744b0be521534c26
https://github.com/Cheramlak-3319 <cheemanbest@gmail.com>
2026-02-16 16:35:44 +0300

    Final clean start: excluding large binaries

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { body } = require("express-validator");
-const AuthController = require("../controllers/AuthController");
+const AuthController = require("../controllers/authController.js");
 
 const registerValidation = [
   body("firstName").notEmpty(),
