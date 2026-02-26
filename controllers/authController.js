ed494dcc1b087ebc328f94f9dd1fe68f9ed81cfb
https://github.com/Cheramlak-3319 <cheemanbest@gmail.com>
2026-02-26 15:58:14 +0300

    describe your changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,7 @@ const generateTokens = (user) => {
   const accessToken = jwt.sign(
     { userId: user._id, email: user.email, role: user.role },
     process.env.JWT_SECRET,
-    { expiresIn: "15m" },
+    { expiresIn: "3m" },
   );
   const refreshToken = jwt.sign(
     { userId: user._id },
